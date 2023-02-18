80fe9ee66f1bd2e79c8a56f820654f325918df07
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2023-02-18 11:27:52 +0900

    Add basic UI elements.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,46 @@
 <script lang="ts">
 	import '@picocss/pico'
+
+	let query = 'QUERY'
 </script>
 
 <main class="container">
-	<h1>Welcome to SvelteKit</h1>
-	<p>
-		Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-	</p>
+	<textarea rows="2" bind:value={query} />
+
+	<div>
+		<span role="button" class="primary">All Translate</span>
+		<span role="button" class="secondary">Google</span>
+		<span role="button" class="secondary">Papago</span>
+	</div>
+
+	<div>
+		<span role="button" class="primary">All Shopping</span>
+		<span role="button" class="secondary">Danawa</span>
+		<span role="button" class="secondary">Naver</span>
+		<span role="button" class="secondary">Kakao</span>
+		<span role="button" class="secondary">Enuri</span>
+	</div>
+
+	<div>
+		<span role="button" class="primary">All Search</span>
+		<span role="button" class="secondary">Google</span>
+		<span role="button" class="secondary">Naver</span>
+		<span role="button" class="secondary">Daum</span>
+		<span role="button" class="secondary">Bing</span>
+		<span role="button" class="secondary">Yandex</span>
+	</div>
 </main>
+
+<style>
+	main > div {
+		margin-bottom: var(--spacing);
+	}
+
+	:is([role='button']).primary {
+		width: 8rem;
+	}
+
+	span {
+		margin-bottom: 0.1rem;
+	}
+</style>
