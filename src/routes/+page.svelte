00fb9733ff3a8447320e702f50fbcea10c38d660
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2023-02-18 18:16:38 +0900

    Allow SHIFT-ENTER to newline instead of translate.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -170,7 +170,7 @@
 	const handleKeydown = (e: KeyboardEvent) => {
 		log('handleKeydown', e)
 
-		if (e.key === 'Enter') {
+		if (e.key === 'Enter' && !e.shiftKey) {
 			e.preventDefault()
 			handleClickAllTranslate(e)
 		}
