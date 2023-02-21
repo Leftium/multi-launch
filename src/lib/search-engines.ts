4ae63e7e3e7d3bcd715e12e4df33f80f1843e425
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2023-02-21 13:10:41 +0900

    Add searchGroupConfigs.
    Also refactor selectUrl() from ifKorean() and ifUrl().

--- src/lib/search-engines.ts
+++ src/lib/search-engines.ts
@@ -1,18 +1,26 @@
 import debugFactory from 'debug'
 const log = debugFactory('log')
 
-export type UrlTemplateOrGenerator = string | ((query: string) => string)
+export type UrlTemplateOrSelector = string | ((query: string) => string)
+
+type UrlConfig = {
+	link?: string
+	lang_ko?: string
+	default: string
+}
+
+type SearchEngineConfig = {
+	url: string | UrlConfig
+	excludeFromAllSearch?: boolean
+}
 
 // This function is not bound to any local variables.
-export const makeClickHandler = (
-	_query: string,
-	urlTemplateOrGenerator: UrlTemplateOrGenerator
-) => {
+export const makeClickHandler = (_query: string, urlTemplateOrSelector: UrlTemplateOrSelector) => {
 	return (e: Event) => {
 		const urlTemplate =
-			typeof urlTemplateOrGenerator === 'string'
-				? urlTemplateOrGenerator
-				: urlTemplateOrGenerator(_query)
+			typeof urlTemplateOrSelector === 'string'
+				? urlTemplateOrSelector
+				: urlTemplateOrSelector(_query)
 
 		const queryTrimmedEncoded = encodeURIComponent(_query.trim())
 		const url = urlTemplate.replace('QUERY', queryTrimmedEncoded)
@@ -25,18 +33,28 @@ export const makeClickHandler = (
 	}
 }
 
-// Makes function that returns different strings depending on if input contains Korean.
-export const ifKorean = (ifKorean: string, ifNotKorean: string) => {
-	const koreanRegex = /[\u3131-\uD79D]/giu // https://stackoverflow.com/a/38156301/117030
-	return (text: string) => {
-		return koreanRegex.test(text) ? ifKorean : ifNotKorean
-	}
-}
-
-// Makes function that returns different strings depending on if input contains a URL.
-export const ifUrl = (ifUrl: string, ifNotUrl: string) => {
+// Makes function that returns a (string) url from config based on query contents.
+// inputs: query, config
+// output: urlTemplate
+export const selectUrl = (config: SearchEngineConfig) => {
 	const urlRegex = /^http/iu
+	const koreanRegex = /[\u3131-\uD79D]/giu // https://stackoverflow.com/a/38156301/117030
+
 	return (text: string) => {
-		return urlRegex.test(text) ? ifUrl : ifNotUrl
+		// Simple URL.
+		if (typeof config.url === 'string') {
+			return config.url
+		}
+
+		let urlTemplate = config.url.default
+
+		if (urlRegex.test(text)) {
+			urlTemplate = config.url.link || urlTemplate
+		}
+
+		if (koreanRegex.test(text)) {
+			urlTemplate = config.url.lang_ko || urlTemplate
+		}
+		return urlTemplate
 	}
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,48 +14,126 @@
 	let textArea: HTMLTextAreaElement
 
 	// Generate click handlers bound to local `query`.
-	const makeClickHandler = (urlTemplateOrGenerator: SE.UrlTemplateOrGenerator) => (e: Event) => {
-		SE.makeClickHandler(query, urlTemplateOrGenerator)(e)
+	const makeClickHandler = (urlTemplateOrSelector: SE.UrlTemplateOrSelector) => (e: Event) => {
+		SE.makeClickHandler(query, urlTemplateOrSelector)(e)
+	}
+
+	const searchGroupConfigs = {
+		Translate: {
+			Google: {
+				url: {
+					link: '',
+					lang_ko: 'https://translate.google.com/?sl=ko&tl=en&text=QUERY&op=translate',
+					default: 'https://translate.google.com/?sl=en&tl=ko&text=QUERY&op=translate',
+				},
+			},
+
+			Papago: {
+				url: {
+					link: 'https://papago.naver.net/website?locale=en&source=auto&target=en&url=QUERY',
+					lang_ko: 'https://papago.naver.com/?sk=ko&tk=en&hn=0&st=QUERY',
+					default: 'https://papago.naver.com/?sk=en&tk=ko&hn=0&st=QUERY',
+				},
+			},
+		},
+
+		Shopping: {
+			Danawa: {
+				url: 'https://search.danawa.com/dsearch.php?k1=QUERY',
+			},
+			Naver: {
+				url: 'https://search.shopping.naver.com/search/all?query=QUERY',
+			},
+			Kakao: { url: 'https://shoppinghow.kakao.com/search/QUERY' },
+			Enuri: { url: 'http://www.enuri.com/search.jsp?keyword=QUERY' },
+			중고나라: { url: 'https://web.joongna.com/search/QUERY' },
+			'중고나라 카페': {
+				url: 'http://cafe.naver.com/joonggonara?iframe_url=/joonggonara.cafe//ArticleSearchList.nhn%3Fsearch.clubid=10050146%26search.searchBy=0%26search.query=QUERY',
+			},
+			Homeplus: { url: 'https://front.homeplus.co.kr/search?keyword=QUERY' },
+		},
+
+		Search: {
+			Google: { url: 'https://www.google.com/search?q=QUERY' },
+			Naver: { url: 'https://search.naver.com/search.naver?query=QUERY' },
+			Daum: { url: 'https://search.daum.net/search?q=QUERY' },
+			Bing: { url: 'https://www.bing.com/search?q=QUERY' },
+			Yandex: { url: 'https://yandex.com/search/?text=QUERY' },
+		},
+
+		Images: {
+			Google: {
+				url: {
+					link: 'https://lens.google.com/uploadbyurl?url=QUERY',
+					default: 'https://www.google.com/search?tbm=isch&q=QUERY',
+				},
+			},
+			Naver: {
+				url: {
+					link: '',
+					default: 'https://search.naver.com/search.naver?where=image&query=QUERY',
+				},
+			},
+			Daum: { url: { link: '', default: 'https://search.daum.net/search?w=img&q=QUERY' } },
+			Bing: {
+				url: {
+					link: 'https://www.bing.com/images/search?view=detailv2&iss=sbi&q=imgurl:QUERY',
+					default: 'https://www.bing.com/images/search?q=QUERY',
+				},
+			},
+			Yandex: {
+				url: {
+					link: 'https://yandex.com/images/search?rpt=imageview&url=QUERY',
+					default: 'https://yandex.com/images/search?text=QUERY',
+				},
+			},
+			Unsplash: { url: { link: '', default: 'https://unsplash.com/s/photos/QUERY' } },
+			Pixabay: { url: { link: '', default: 'https://pixabay.com/images/search/QUERY' } },
+			Pexels: { url: { link: '', default: 'https://www.pexels.com/search/QUERY/' } },
+			StockUnltd: {
+				url: {
+					link: '',
+					default: 'https://www.stockunlimited.com/vector-image/?word=QUERY',
+				},
+			},
+			YayImages: {
+				url: { link: '', default: 'https://yayimages.com/search?type=-1&phrase=QUERY' },
+			},
+		},
 	}
 
 	const handleClickGoogleTranslate = makeClickHandler(
-		SE.ifKorean(
-			'https://translate.google.com/?sl=ko&tl=en&text=QUERY&op=translate',
-			'https://translate.google.com/?sl=en&tl=ko&text=QUERY&op=translate'
-		)
+		SE.selectUrl(searchGroupConfigs.Translate.Google)
 	)
 
-	const handleClickPapago = makeClickHandler(
-		SE.ifKorean(
-			'https://papago.naver.com/?sk=ko&tk=en&hn=0&st=QUERY',
-			'https://papago.naver.com/?sk=en&tk=ko&hn=0&st=QUERY'
-		)
-	)
+	const handleClickPapago = makeClickHandler(SE.selectUrl(searchGroupConfigs.Translate.Papago))
 
 	const handleClickAllTranslate = (e: Event) => {
 		handleClickGoogleTranslate(e)
 		handleClickPapago(e)
 	}
 
-	const handleClickDanawa = makeClickHandler('https://search.danawa.com/dsearch.php?k1=QUERY')
+	const handleClickDanawa = makeClickHandler(SE.selectUrl(searchGroupConfigs.Shopping.Danawa))
 
 	const handleClickNaverShopping = makeClickHandler(
-		'https://search.shopping.naver.com/search/all?query=QUERY'
+		SE.selectUrl(searchGroupConfigs.Shopping.Naver)
 	)
 
-	const handleClickKakaoShopping = makeClickHandler('https://shoppinghow.kakao.com/search/QUERY')
+	const handleClickKakaoShopping = makeClickHandler(
+		SE.selectUrl(searchGroupConfigs.Shopping.Kakao)
+	)
 
-	const handleClickEnuri = makeClickHandler('http://www.enuri.com/search.jsp?keyword=QUERY')
+	const handleClickEnuri = makeClickHandler(SE.selectUrl(searchGroupConfigs.Shopping.Enuri))
 
-	const handleClickJoonggoNara = makeClickHandler('https://web.joongna.com/search/QUERY')
+	const handleClickJoonggoNara = makeClickHandler(
+		SE.selectUrl(searchGroupConfigs.Shopping.중고나라)
+	)
 
 	const handleClickJoonggoNaraCafe = makeClickHandler(
-		'http://cafe.naver.com/joonggonara?iframe_url=/joonggonara.cafe//ArticleSearchList.nhn%3Fsearch.clubid=10050146%26search.searchBy=0%26search.query=QUERY'
+		SE.selectUrl(searchGroupConfigs.Shopping['중고나라 카페'])
 	)
 
-	const handleClickHomeplus = makeClickHandler(
-		'https://front.homeplus.co.kr/search?keyword=QUERY'
-	)
+	const handleClickHomeplus = makeClickHandler(SE.selectUrl(searchGroupConfigs.Shopping.Homeplus))
 
 	const handleClickAllShopping = (e: Event) => {
 		handleClickHomeplus(e)
@@ -67,11 +145,11 @@
 		handleClickDanawa(e)
 	}
 
-	const handleClickGoogle = makeClickHandler('https://www.google.com/search?q=QUERY')
-	const handleClickNaver = makeClickHandler('https://search.naver.com/search.naver?query=QUERY')
-	const handleClickDaum = makeClickHandler('https://search.daum.net/search?q=QUERY')
-	const handleClickBing = makeClickHandler('https://www.bing.com/search?q=QUERY')
-	const handleClickYandex = makeClickHandler('https://yandex.com/search/?text=QUERY')
+	const handleClickGoogle = makeClickHandler(SE.selectUrl(searchGroupConfigs.Search.Google))
+	const handleClickNaver = makeClickHandler(SE.selectUrl(searchGroupConfigs.Search.Naver))
+	const handleClickDaum = makeClickHandler(SE.selectUrl(searchGroupConfigs.Search.Daum))
+	const handleClickBing = makeClickHandler(SE.selectUrl(searchGroupConfigs.Search.Bing))
+	const handleClickYandex = makeClickHandler(SE.selectUrl(searchGroupConfigs.Search.Yandex))
 
 	const handleClickAllSearch = (e: Event) => {
 		handleClickYandex(e)
@@ -81,52 +159,18 @@
 		handleClickGoogle(e)
 	}
 
-	const handleClickGoogleImage = makeClickHandler(
-		SE.ifUrl(
-			'https://lens.google.com/uploadbyurl?url=QUERY',
-			'https://www.google.com/search?tbm=isch&q=QUERY'
-		)
-	)
-
-	const handleClickYandexImage = makeClickHandler(
-		SE.ifUrl(
-			'https://yandex.com/images/search?rpt=imageview&url=QUERY',
-			'https://yandex.com/images/search?text=QUERY'
-		)
-	)
-
-	const handleClickBingImage = makeClickHandler(
-		SE.ifUrl(
-			'https://www.bing.com/images/search?view=detailv2&iss=sbi&q=imgurl:QUERY',
-			'https://www.bing.com/images/search?q=QUERY'
-		)
-	)
-
-	const handleClickNaverImage = makeClickHandler(
-		SE.ifUrl('', 'https://search.naver.com/search.naver?where=image&query=QUERY')
-	)
-
-	const handleClickDaumImage = makeClickHandler(
-		SE.ifUrl('', 'https://search.daum.net/search?w=img&q=QUERY')
-	)
-
-	const handleClickUnsplash = makeClickHandler(
-		SE.ifUrl('', 'https://unsplash.com/s/photos/QUERY')
-	)
-
-	const handleClickPixabay = makeClickHandler(
-		SE.ifUrl('', 'https://pixabay.com/images/search/QUERY')
-	)
-
-	const handleClickPexels = makeClickHandler(SE.ifUrl('', 'https://www.pexels.com/search/QUERY/'))
-
+	const handleClickGoogleImage = makeClickHandler(SE.selectUrl(searchGroupConfigs.Images.Google))
+	const handleClickYandexImage = makeClickHandler(SE.selectUrl(searchGroupConfigs.Images.Yandex))
+	const handleClickBingImage = makeClickHandler(SE.selectUrl(searchGroupConfigs.Images.Bing))
+	const handleClickNaverImage = makeClickHandler(SE.selectUrl(searchGroupConfigs.Images.Naver))
+	const handleClickDaumImage = makeClickHandler(SE.selectUrl(searchGroupConfigs.Images.Daum))
+	const handleClickUnsplash = makeClickHandler(SE.selectUrl(searchGroupConfigs.Images.Unsplash))
+	const handleClickPixabay = makeClickHandler(SE.selectUrl(searchGroupConfigs.Images.Pixabay))
+	const handleClickPexels = makeClickHandler(SE.selectUrl(searchGroupConfigs.Images.Pexels))
 	const handleClickStockUnlimited = makeClickHandler(
-		SE.ifUrl('', 'https://www.stockunlimited.com/vector-image/?word=QUERY')
+		SE.selectUrl(searchGroupConfigs.Images.StockUnltd)
 	)
-
-	const handleClickYayImages = makeClickHandler(
-		SE.ifUrl('', 'https://yayimages.com/search?type=-1&phrase=QUERY')
-	)
+	const handleClickYayImages = makeClickHandler(SE.selectUrl(searchGroupConfigs.Images.YayImages))
 
 	const handleClickAllImages = (e: Event) => {
 		handleClickYayImages(e)
