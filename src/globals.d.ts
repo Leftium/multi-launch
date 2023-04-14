755821ca36911e4b3172c012b2a715f43f847816
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2023-04-14 17:10:25 -0500

    Delete most files.

--- src/globals.d.ts
+++
@@ -1,7 +0,0 @@
-// https://github.com/pngwn/MDsveX/discussions/369
-declare module '*.md'
-
-declare module '*.toml' {
-	const value: unknown
-	export default value
-}
--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { sveltekit } from '@sveltejs/kit/vite'
-import { ViteToml } from 'vite-plugin-toml'
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-	plugins: [sveltekit(), ViteToml()],
-})
