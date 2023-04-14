1749fd718bb2d001e094479290d95d0d33b5fe38
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2023-04-14 17:58:35 -0500

    Read and display default.toml.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,10 @@
 	import debugFactory from 'debug'
 	const log = debugFactory('log')
 	log('Hello')
+
+	import defaultPlanToml from '$lib/plans/default.toml?raw'
 </script>
 
 <main class="container">
-	<h1>Welcome to SvelteKit</h1>
-	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<pre>{defaultPlanToml}</pre>
 </main>
