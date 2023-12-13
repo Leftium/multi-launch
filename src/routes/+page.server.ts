2a74897d1da9dd130733990a21552c4ba93ce1a4
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2023-12-13 13:43:59 +0900

    Need longer bypass token.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -21,7 +21,7 @@ export const config = {
 		// with a __prerender_bypass=<token> cookie.
 		//
 		// Making a `GET` or `HEAD` request with `x-prerender-revalidate: <token>` will force the asset to be re-validated.
-		bypassToken: 'BYPASSBYPASSBYPASSBYPASS',
+		bypassToken: 'bypass-bypass-bypass-bypass-bypass',
 
 		// List of valid query parameters. Other parameters (such as utm tracking codes) will be ignored,
 		// ensuring that they do not result in content being regenerated unnecessarily
